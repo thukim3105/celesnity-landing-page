37423acb8b08cc04ac993db5fd8dc3027e53acb5
doanthukim <thukim3105@gmail.com>
2026-07-20 01:25:19 +0700

    Set fac-fly as the homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,20 +1,2 @@
-import { setRequestLocale } from "next-intl/server";
-import { Hero } from "@/components/hero/Hero";
-import { Capabilities } from "@/components/capabilities/Capabilities";
-import { CTA } from "@/components/cta/CTA";
-
-export default async function Home({
-  params,
-}: {
-  params: Promise<{ locale: string }>;
-}) {
-  const { locale } = await params;
-  setRequestLocale(locale);
-  return (
-    <main style={{ position: "relative", zIndex: 1 }}>
-      <Hero />
-      <Capabilities />
-      <CTA />
-    </main>
-  );
-}
+// The default locale route (/) uses the same experience as /fac-fly.
+export { default } from "./fac-fly/page";
